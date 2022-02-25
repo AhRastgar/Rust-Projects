fa1057a1fb60fea2302410d121e741079cca7373
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-25 17:43:01 +0330

    .

---
+++ Draw a triangle a.rs
@@ -0,0 +1,25 @@
+fn main ()
+{
+    // input
+    println! ("Enter a number for draw triangle a ");
+    let x = get_input ();
+    
+    // the operation & output
+    for i in 0..x + 1
+    {
+        for j in 0..i
+        {
+            print! ("* ");
+        }
+        print! ("\n");
+    }
+    print! ("\n");
+}
+
+fn get_input() -> i32
+{
+    let mut line  = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    let number : i32 = line.trim().parse().unwrap();
+    return number ;
+}
