a5030ca8d97a44462f25522b5e04ea7bd2b1231b
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-02-25 13:36:59 +0330

    .

---
+++ Check triangle.rs
@@ -0,0 +1,26 @@
+fn main ()
+{
+    println! ("Enter a chorm ");
+    let a = get_input ();
+    println! ("Enter a Opposite to the chord ");
+    let b = get_input ();
+    println! ("Enter a Adjacent to the chord ");
+    let c = get_input ();
+    
+    if a > b + c
+    {
+        print! ("This is not a triangle.");
+    }
+    else
+    {
+        print! ("This is a triangle.");
+    }
+}
+
+fn get_input() -> i32{
+
+    let mut line  = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    let number : i32 = line.trim().parse().unwrap();
+    return number ;
+}
