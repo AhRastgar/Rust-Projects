f678e864246b977fdeb3a734419c710af8e0b495
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-04-03 14:40:20 +0430

    .

---
+++ Hollow square.rs
@@ -0,0 +1,36 @@
+fn main ()
+{
+    println!("This project is for draw a hollow square.");
+    let num = get_input;
+    
+    for i in 0..num
+    {
+        print! ("* ");
+    }
+    print! ("\n");
+    
+    for j in 0..num - 2
+    {
+        print! ("*");
+        for h in 0..2 * num - 3
+        {
+            print! (" ");
+        }
+        print! ("*");
+    }
+    
+    for z in 0..num
+    {
+        print! ("* ");
+    }
+    
+    return 0;
+}
+
+fn get_input() -> i32
+{
+    let mut line  = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+    let number : i32 = line.trim().parse().unwrap();
+    return number ;
+}
